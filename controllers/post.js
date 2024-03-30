23b45464823e80098f8b50a85035478a1df1219a
catsociety <aliu6876@gmail.com>
2024-03-29 23:07:02 -0400

    passing 46/52 tests

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,30 @@
 const { Post, Tag } = require('../models')
 
 async function create(req, res, next) {
-  const {title, body, tags} = req.body
-  // TODO: create a new post
-  // if there is no title or body, return a 400 status
-  // omitting tags is OK
-  // create a new post using title, body, and tags
-  // return the new post as json and a 200 status
+  try {
+    const {title, body, tags} = req.body
+    // TODO: create a new post
+    // if there is no title or body, return a 400 status
+    // omitting tags is OK
+    if (!(
+      title &&
+      body
+    ))
+      return res
+        .status(400)
+        .send('Must include title and body.')
+    
+    // create a new post using title, body, and tags
+    const post = await Post.create({
+      title, body, tags
+    })
+    // return the new post as json and a 200 status
+    res.status(200).json(post)
+
+  } catch (err) {
+    res.status(500).send('Error creating post: ' + err.message)
+  }
+  
 }
 
 // should render HTML
@@ -16,6 +34,8 @@ async function get(req, res) {
     // TODO: Find a single post
     // find a single post by slug and populate 'tags'
     // you will need to use .lean() or .toObject()
+    const post = await Post.findById(slug).populate('tags').lean()
+
     post.createdAt = new Date(post.createdAt).toLocaleString('en-US', {
       month: '2-digit',
       day: '2-digit',
@@ -78,17 +98,35 @@ async function update(req, res) {
     // TODO: update a post
     // if there is no title or body, return a 400 status
     // omitting tags is OK
+    if (!(
+      title &&
+      body
+    ))
+      return res
+        .status(400)
+        .send('Must include title and body.')
     // find and update the post with the title, body, and tags
+    const post = await Post.findByIdAndUpdate(postId, {title, body, tags})
     // return the updated post as json
+    res.json(post)
+
   } catch(err) {
     res.status(500).send(err.message)
   }
 }
 
 async function remove(req, res, next) {
-  const postId = req.params.id
-  // TODO: Delete a post
-  // delete post by id, return a 200 status
+  try {
+    const postId = req.params.id
+    // TODO: Delete a post
+    // delete post by id, return a 200 status
+    const post = await Post.findByIdAndDelete(postId)
+    if (!post) return res.status(404).send('Post not found.')
+      res.status(200).json(post)
+
+  } catch (err) {
+    res.status(500).send('Error deleting post: ' + err.message)
+  }
 }
 
 module.exports = {
